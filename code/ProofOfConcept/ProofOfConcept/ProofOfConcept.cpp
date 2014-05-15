fc39f7d70220e5cc7bf2cda8ba899034391adfd9
Raymond <Rayrohder@gmail.com>
2014-05-15 12:56:54 +0200

    poc initial push

--- code/ProofOfConcept/ProofOfConcept/ProofOfConcept.cpp
+++ code/ProofOfConcept/ProofOfConcept/ProofOfConcept.cpp
@@ -5,6 +5,7 @@
 using namespace cv;
 
 Mat image;
+Mat gray;
 
 int main() {
 
@@ -14,15 +15,16 @@ int main() {
 
 	// create window to show image
 	namedWindow("window", WINDOW_AUTOSIZE);
+	namedWindow("gray", CV_WINDOW_AUTOSIZE);
 	moveWindow("window", 400, 100);
 	while (1) {
 
 		// cop webcam stream to image
 		cap >> image;
-
+		cvtColor(image, gray, CV_BGR2HSV);
 		// print image to screen
 		imshow("window", image);
-
+		imshow("gray", gray);
 		// delay 33 ms
 		waitKey(33);
 	}
